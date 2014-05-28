df242434cca0ae6f9c89649c3ed7d1c545521313
Kai Strempel <kstrempel@gmail.com>
2014-05-28 20:28:37 +0200

    Added data for similar user

---
+++ MakingRecommendations/similarusers/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to similarusers
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ MakingRecommendations/similarusers/test/similarusers/core_test.clj
@@ -0,0 +1,7 @@
+(ns similarusers.core-test
+  (:require [clojure.test :refer :all]
+            [similarusers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

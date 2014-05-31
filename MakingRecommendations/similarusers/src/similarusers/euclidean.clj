c1059d23281f4ae2ed5f8f2d1cb25e68e1eef84d
Kai Strempel <kstrempel@gmail.com>
2014-05-31 11:21:14 +0200

    refactor euclidean version

--- MakingRecommendations/similarusers/src/similarusers/euclidean.clj
+++ MakingRecommendations/similarusers/src/similarusers/euclidean.clj
@@ -5,22 +5,22 @@
 
 (defn get_critic 
   [prefs person movie]
-  (get (get prefs person) movie))
+  (-> (get prefs person)
+      (get movie)))
 
 (defn sim_distance
   [prefs person1 person2]
-  (let [items_p1 (into #{} (keys (get prefs person1)))
-        items_p2 (into #{} (keys (get prefs person2)))
-        items    (intersection items_p1 items_p2)]
-    (/ 1 
-       (+ 1
-          (reduce + (map #(expt (- (get_critic prefs person1 %)
-                                (get_critic prefs person2 %))
-                                2)
-                         items))))))
-
-(defn -main
-  [& args]
-  (println "Hello World"))
+  (let [getter    (partial get_critic prefs)
+        movies_p1 (into #{} (keys (get prefs person1)))
+        movies_p2 (into #{} (keys (get prefs person2)))
+        movies    (intersection movies_p1 movies_p2)]
+    (->> 
+     (reduce + (map #(expt (- (getter person1 %)
+                              (getter person2 %))
+                           2)
+                    movies))
+     (+ 1)
+     (/ 1))))
+  
 
 
